a24b772751aca293f51a414bdeddf539bde3d7d6
Bobi2000 <bobko_2000@abv.bg>
2020-03-06 19:35:38 +0200

    Creating trees folders
    First init

---
+++ GIL_HACKATHON2020/Assets/Client/Scripts/Networking/ClientController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
